2ef604abd48f57e33926bf4159a87234c817724c
KevinK <gkangkai@gmail.com>
2018-10-08 21:33:04 +0800

    Add: Read rooms data

--- index.js
+++ index.js
@@ -15,20 +15,54 @@ let main = new KRouter();
 let rooms = new KRouter({ prefix: '/rooms' });
 let acts = new KRouter({ prefix: '/acts' });
 
+let dataRooms = [];
+
+const read = function (filename) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filename, 'utf-8', (err, data) => {
+            if (err) reject(err);
+            else resolve(data);
+        });
+    });
+};
+
+const write = function (filename, content) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(filename, 'utf-8', (err) => {
+            if (err) reject(err);
+            else resolve();
+        });
+    });
+};
+
 main.get('/', async ctx => {
     ctx.response.type = 'html';
     ctx.response.body = fs.createReadStream('./static/hello.html');
 });
 
 rooms.get('/', async ctx => {
-    // TODO
-    ctx.response.body = 'Rooms';
+    try {
+        dataRooms = JSON.parse(await read('data/rooms.json'));
+        ctx.response.body = dataRooms;
+    } catch (e) {
+        console.log(e);
+        ctx.throw(500);
+    }
 });
 
-rooms.get('/:rid/month/:month', async ctx => {
+acts.get('/month/:month', async ctx => {
     // TODO
-    // ctx.toJSON({});
-    ctx.response.body = ctx.params.rid + ' ' + ctx.params.month;
+    let rid = ctx.params.rid;
+    let month = ctx.params.month;
+    if (! dataRooms.some(r => r.id === rid) || ! /^\d{4}-\d{2}$/.test(month)) {
+        ctx.throw(404); return;
+    }
+    let y = month.substr(0, 4), m = month.substr(5, 2);
+    let r = [];
+    try {
+        r = JSON.parse(await read(`data/room1/${y}-${m}.json`));
+    } catch (e) {}
+    ctx.response.body = r;
 });
 
 acts.get('/:aid', async ctx => {
