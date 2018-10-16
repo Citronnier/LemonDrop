e142144f4bd8fe4760e226112861c452b219b5e7
KevinK <gkangkai@gmail.com>
2018-10-16 22:09:27 +0800

    Add: Get activities in current month

--- static/main.js
+++ static/main.js
@@ -2,19 +2,35 @@ $(document).ready(function () {
     get('rooms', function (res) {
         var rooms = JSON.parse(res);
         $('#roomlist').empty();
-        for (var r of rooms)
-            $('#roomlist').append('<a>' + r.name + '</a>');
+        for (var r of rooms) {
+            $('#roomlist').append('<a id="room-' + r.id + '" href="#">' + r.name + '</a>');
+            $('#room-' + r.id).on('click', null, r.id, getActs);
+        }
     });
 });
 
-function get(url, fn) {
+function getActs(e) {
+    var roomid = e.data;
+    var year = new Date().getFullYear();
+    var month = new Date().getMonth() + 1;
+    get('rooms/' + roomid + '/acts/month/' + year + '-' + month, function (res) {
+        var acts = JSON.parse(res);
+        $('#actlist').empty();
+        for (var act of acts) {
+            $('#actlist').append('<p>' + JSON.stringify(act) + '</p>');
+        }
+    });
+    return false;
+};
+
+function get(url, success, fail) {
     var req = new XMLHttpRequest();
     req.onreadystatechange = function () {
         if (req.readyState === 4) {
             if (req.status === 200) {
-                fn(req.responseText);
-            } else {
-                // (req.status);
+                success(req.responseText);
+            } else if (fail) {
+                fail(req.status);
             }
         }
     }
